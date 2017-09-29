65f33e48c63be11f360f014e36293fcab57c215f
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-09-28 20:51:59 -0400

    WP Work Order Proc: createWorkOrders written & writing Obj to .json.

--- src/com/sardina/Creator.java
+++ src/com/sardina/Creator.java
@@ -1,32 +1,56 @@
 package com.sardina;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Creator extends WorkOrder {
+public class Creator extends WorkOrder{
 
     public void createWorkOrders() {
         //TODO: read input, create work orders and write as json files
+        Scanner scanner = new Scanner(System.in);
+
+        //ask for input, read, set as variables
+        System.out.println("Enter your name: ");
+            String woSenderName = scanner.nextLine();
+        System.out.println("Enter a work order description: ");
+            String woDescription = scanner.nextLine();
+        System.out.println("Enter a new 5 digit work order number # # # # # : ");
+            int woId = scanner.nextInt();
+
+        //create w/o
+        WorkOrder createWrkOrder = new WorkOrder();
+            createWrkOrder.setId(woId);
+            createWrkOrder.setDescription(woDescription);
+            createWrkOrder.setSenderName(woSenderName);
+            createWrkOrder.setStatus(Status.INITIAL);
+
+        //write w/o as .json
+        String jsonWorkOrder = "";
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            jsonWorkOrder = mapper.writeValueAsString(createWrkOrder);
+        } catch (IOException ex) {
+            ex.printStackTrace(); }
+
+        //Create VIN.json file for the new instance of vehicleInfo
+        try {
+            File jsonFile = new File(createWrkOrder.getId() + ".json");
+            FileWriter newFile = new FileWriter(jsonFile);
+
+            newFile.write(jsonWorkOrder);
+            newFile.close();
+        } catch (IOException ex) {
+            ex.printStackTrace(); }
 
     }
 
     public static void main(String args[]) {
         Creator creator = new Creator();
         creator.createWorkOrders();
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a new 5 digit work order number # # # # # : ");
-            int woId = scanner.nextInt();
-        System.out.println("Enter a work order description: ");
-            String woDescription = scanner.nextLine();
-        System.out.println("Enter your name: ");
-            String woSenderName = scanner.nextLine();
-
-        creator.setId(woId);
-        creator.setDescription(woDescription);
-        creator.setSenderName(woSenderName);
-        creator.setStatus(Status.INITIAL);
-
     }
 
 
